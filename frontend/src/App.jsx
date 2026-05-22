e7b6227205b10b0bd0e3548dc820028f543558cb
Radhika daslaniya <radhupatel0604@gmail.com>
2026-05-22 13:05:15 +0530

    added navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,57 @@
+import { useState } from "react";
 import Login from "./Login";
 import Register from "./Register";
 import Dashboard from "./Dashboard";
 
 function App() {
+  const [page, setPage] = useState("login");
+
   return (
-    <div>
-      <Login />
-      <hr />
-
-      <Register />
-      <hr />
-
-      <Dashboard />
-      <hr />
-
+    <div style={{ textAlign: "center" }}>
       <h1>Team Task Manager</h1>
 
-      <input placeholder="Task Name" />
-      <br /><br />
+      {/* Navigation Buttons */}
+      <button onClick={() => setPage("login")}>Login</button>
 
-      <input placeholder="Assigned To" />
-      <br /><br />
+      <button onClick={() => setPage("register")}>
+        Register
+      </button>
 
-      <button>Create Task</button>
+      <button onClick={() => setPage("task")}>
+        Task Manager
+      </button>
 
-      <h2>Tasks</h2>
+      <button onClick={() => setPage("dashboard")}>
+        Dashboard
+      </button>
 
-      <p>Login Page - Aarush</p>
-      <p>Registration - Ansh</p>
+      <hr />
+
+      {/* Pages */}
+      {page === "login" && <Login />}
+
+      {page === "register" && <Register />}
+
+      {page === "task" && (
+        <div>
+          <h2>Create Task</h2>
+
+          <input placeholder="Task Name" />
+          <br /><br />
+
+          <input placeholder="Assigned To" />
+          <br /><br />
+
+          <button>Create Task</button>
+
+          <h2>Tasks</h2>
+
+          <p>Login Page - Aarush</p>
+          <p>Registration - Ansh</p>
+        </div>
+      )}
+
+      {page === "dashboard" && <Dashboard />}
     </div>
   );
 }
