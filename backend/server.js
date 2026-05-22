a4bcb4c099feba2053cafe6da48c684a80de8792
Radhika daslaniya <radhupatel0604@gmail.com>
2026-05-22 09:42:55 +0530

    fixed package

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ app.get("/", (req, res) => {
   res.send("Backend Running");
 });
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
